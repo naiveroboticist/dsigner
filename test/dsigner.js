25566465d703292703062947bacc813b5314e49d
Naive Roboticist <naiveroboticist@gmail.com>
2016-05-27 17:05:02 -0400

    Now conforms to standard and tested with v6.1.0.

--- test/dsigner.js
+++ test/dsigner.js
@@ -1,67 +1,62 @@
-var expect = require('chai').expect;
-var dsigner = require('../lib/dsigner');
-var sinon = require('sinon');
-var crypto = require('crypto');
-var fs = require('fs');
+'use strict'
 
-describe("dsigner", function() {
-  describe(".createPrivateKeyPath", function() {
+const expect = require('chai').expect
+const dsigner = require('../lib/dsigner')
+const sinon = require('sinon')
+const crypto = require('crypto')
 
-    it("should generate a valid private key path given a base path and name", function() {
-      var keyPath = dsigner.createPrivateKeyPath("/root", "key_name");
+describe('dsigner', function () {
+  describe('.createPrivateKeyPath', function () {
+    it('should generate a valid private key path given a base path and name', function () {
+      let keyPath = dsigner.createPrivateKeyPath('/root', 'key_name')
 
-      expect(keyPath).to.equal("/root/key_name_private.pem");
-    });
+      expect(keyPath).to.equal('/root/key_name_private.pem')
+    })
+  })
 
-  });
+  describe('.createPublicKeyPath', function () {
+    it('should generate a valid public key path given a base path and name', function () {
+      let keyPath = dsigner.createPublicKeyPath('/root', 'key_name')
 
-  describe(".createPublicKeyPath", function() {
+      expect(keyPath).to.equal('/root/key_name_public.pem')
+    })
+  })
 
-    it("should generate a valid public key path given a base path and name", function() {
-      var keyPath = dsigner.createPublicKeyPath("/root", "key_name");
+  describe('.signMessage', function () {
+    it('should actually perform the crypto signing of the message', function () {
+      let signer = {}
+      signer.update = function () {}
+      signer.sign = function () {}
 
-      expect(keyPath).to.equal("/root/key_name_public.pem");
-    });
+      let createSign = sinon.stub(crypto, 'createSign').withArgs('RSA-SHA256').returns(signer)
+      let signerUpdate = sinon.stub(signer, 'update').withArgs('the message')
+      let signerSign = sinon.stub(signer, 'sign').withArgs('key data', 'base64').returns('the_signature')
 
-  });
+      let signature = dsigner.signMessage('key data', 'the message')
 
-  describe(".signMessage", function() {
+      expect(signature).to.equal('the_signature')
+      expect(createSign.called)
+      expect(signerUpdate.called)
+      expect(signerSign.called)
+    })
+  })
 
-    it("should actually perform the crypto signing of the message", function() {
-      var signer = {};
-      signer.update = function() {};
-      signer.sign = function() {};
+  describe('.verifySignature', function () {
+    it('should actually perform the crypto verification of the signature', function () {
+      let verifier = {}
+      verifier.update = function () {}
+      verifier.verify = function () {}
 
-      var createSign = sinon.stub(crypto, "createSign").withArgs('RSA-SHA256').returns(signer);
-      var signerUpdate = sinon.stub(signer, "update").withArgs("the message");
-      var signerSign = sinon.stub(signer, "sign").withArgs("key data", "base64").returns("the_signature");
+      let createVerify = sinon.stub(crypto, 'createVerify').withArgs('RSA-SHA256').returns(verifier)
+      let verifierUpdate = sinon.stub(verifier, 'update').withArgs('the message')
+      let verifierVerify = sinon.stub(verifier, 'verify').withArgs('key data', 'signature', 'base64').returns(true)
 
-      var signature = dsigner.signMessage("key data", "the message");
+      let result = dsigner.verifySignature('key data', 'the message', 'signature')
 
-      expect(signature).to.equal("the_signature");
-      expect(createSign.called);
-      expect(signerUpdate.called);
-      expect(signerSign.called);
-    });
-  });
-
-  describe(".verifySignature", function() {
-
-    it("should actually perform the crypto verification of the signature", function() {
-      var verifier = {};
-      verifier.update = function() {};
-      verifier.verify = function() {};
-
-      var createVerify = sinon.stub(crypto, "createVerify").withArgs('RSA-SHA256').returns(verifier);
-      var verifierUpdate = sinon.stub(verifier, "update").withArgs("the message");
-      var verifierVerify = sinon.stub(verifier, "verify").withArgs("key data", "signature", 'base64').returns(true);
-
-      var result = dsigner.verifySignature("key data", "the message", "signature");
-
-      expect(result).to.equal(true);
-      expect(createVerify.called);
-      expect(verifierUpdate.called);
-      expect(verifierVerify.called);
-    });
-  });
-});
+      expect(result).to.equal(true)
+      expect(createVerify.called)
+      expect(verifierUpdate.called)
+      expect(verifierVerify.called)
+    })
+  })
+})
